3e6f70c41d466db2bbb622d31d2bea09d48b8850
z4cope <63551733+z4cope@users.noreply.github.com>
2023-01-24 15:45:48 +0200

    Add indexes to ease searching with queries

---
+++ queries.sql
@@ -0,0 +1,7 @@
+CREATE INDEX patient_id_index ON medical_histories;
+
+CREATE INDEX medical_history_id_index ON invoices;
+
+CREATE INDEX invoice_id_index ON invoice_item;
+
+CREATE INDEX treatment_id_index ON invoice_item;
